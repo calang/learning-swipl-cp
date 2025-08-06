ea77ec2d9c8bf3e0e22e9fc5835ddcf89ce35bff
Carlos A. Lang-Sanou <carlos.lang@gmail.com>
2025-08-06 11:02:26 -0600

    convert to Prolog and rename

---
+++ src/module/validation.pl
@@ -0,0 +1,112 @@
+% validación de condiciones iniciales en la instancia a resolver
+
+:- module(validation, [
+    ningun_resto_es_negativo/0,
+    suma_lecciones/0,
+    todo_req_con_profesor/0,
+    ningun_profesor_mayor_a_cubrir/0
+    ]).
+
+:- use_module([
+    src(sap_prim),
+    module(timetable_base)
+    ]).
+
+
+% ningún nivel debe tener un resto negativo
+ningun_resto_es_negativo :-
+    findall(
+        (Nivel, Resto),
+        (   req(Nivel, resto, Resto),
+            Resto #< 0
+        ),
+        Niveles
+    ),
+    Niveles \= [], !,
+    format('Error: nivel(es) con resto negativo: ~w~n', [Niveles]),
+    fail.
+ningun_resto_es_negativo.
+
+
+% suma_lecciones_por_nivel(+Nivel, ?SumaLecc)
+% SumaLecc es la suma de lecciones requeridas por nivel Nivel.
+%
+suma_lecciones_por_nivel(Nivel, SumaLecc) :-
+    findall(
+        Cantidad,
+        req(Nivel, _Materia, Cantidad),
+        Lecciones
+    ),
+    sum_list(Lecciones, SumaLecc).
+
+
+% ninguna suma de lecciones por nivel debe ser distinta de lecc_por_sem
+suma_lecciones :-
+    lecc_por_sem(LeccPorSem),
+    findall(
+        (Nivel, SumaLecc),
+        (   suma_lecciones_por_nivel(Nivel, SumaLecc),
+            SumaLecc #\= LeccPorSem
+        ),
+        Sumas
+    ),
+    Sumas \= [], !,
+    format('Error: suma de lecciones por nivel distinta de lecc_por_sem: ~w~n', [Sumas]),
+    fail.
+suma_lecciones.
+
+
+% a ningún requisito debe faltarle un profesor que lo pueda cubrir
+todo_req_con_profesor :-
+    findall(
+        (Nivel, Materia),
+        (   req(Nivel, Materia, _),
+            \+ prof(_, Nivel, Materia)
+        ),
+        Reqs
+    ),
+    Reqs \= [], !,
+    format('Error: requisitos sin profesor asignado: ~w~n', [Reqs]),
+    fail.
+todo_req_con_profesor.
+
+
+% prof_cubriendo_lecciones(+Profesor, ?Cantidad)
+% Cantidad es la cantidad de lecciones que el Profesor puede cubrir.
+prof_cubriendo_lecciones(Profesor, Cantidad) :-
+    findall(
+        Cant,
+        (   prof(Profesor, Nivel, Materia),
+            req(Nivel, Materia, Cant)
+        ),
+        ListCant
+    ),
+    sum_list(ListCant, Cantidad).
+
+
+% prof_disponible(+Profesor, ?Cantidad)
+% Cantidad es la cantidad de lecciones en las que el Profesor está disponible.
+prof_disponible(Profesor, Cantidad) :-
+    findall(
+        Lecc,
+        disp(Profesor, _Dia, _Bloque, Lecc),
+        ListLecc
+    ),
+    length(ListLecc, Cantidad).
+
+
+% ningún profesor debe estar asignado a una cantidad de materias mayor a la que puede cubrir
+ningun_profesor_mayor_a_cubrir :-
+    findall(
+        (Profesor, CantReq, CantidadDispo),
+        (   prof(Profesor, _Nivel, _Materia),
+            prof_cubriendo_lecciones(Profesor, CantReq),
+            prof_disponible(Profesor, CantidadDispo),
+            CantReq > CantidadDispo
+        ),
+        ProfCant
+    ),
+    ProfCant \= [], !,
+    format('Error: profesor(es) con cantidad de materias mayor a lo que puede cubrir: ~w~n', [ProfCant]),
+    fail.
+ningun_profesor_mayor_a_cubrir.
