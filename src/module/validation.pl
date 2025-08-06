49fc258ccc77d480f973e82bfc256803aca9388e
Carlos A. Lang-Sanou <carlos.lang@gmail.com>
2025-08-06 15:00:27 -0600

    Separate error detection from error reporting predicates

--- src/module/validation.pl
+++ src/module/validation.pl
@@ -1,11 +1,16 @@
 % validación de condiciones iniciales en la instancia a resolver
 
 :- module(validation, [
+    restos_negativos/1,
     ningun_resto_es_negativo/0,
+    suma_lecciones_por_nivel/2,
     suma_lecciones/0,
+    req_sin_profesor/2,
     todo_req_con_profesor/0,
-    ningun_profesor_mayor_a_cubrir/0
-    ]).
+    prof_cubriendo_lecciones/2,
+    prof_disponible/2,
+    ningun_profesor_sin_poder_cubrir/0
+]).
 
 :- use_module([
     src(sap_prim),
@@ -13,19 +18,26 @@
     ]).
 
 
-% ningún nivel debe tener un resto negativo
-ningun_resto_es_negativo :-
+% restos_negativos(-[(Nivel, Resto), ... ]).
+% Nivel tiene un Resto negativo.
+restos_negativos(Restos) :-
     findall(
         (Nivel, Resto),
         (   req(Nivel, resto, Resto),
-            Resto #< 0
+            Resto < 0
         ),
-        Niveles
-    ),
-    Niveles \= [], !,
-    format('Error: nivel(es) con resto negativo: ~w~n', [Niveles]),
-    fail.
-ningun_resto_es_negativo.
+        Restos
+    ).
+
+
+% ningún nivel debe tener un resto negativo
+ningun_resto_es_negativo :-
+    restos_negativos(Restos),
+    ( Restos = [] ->
+        true
+    ;   format('Error: nivel(es) con resto negativo: ~w~n', [Restos]),
+        fail
+    ).
 
 
 % suma_lecciones_por_nivel(+Nivel, ?SumaLecc)
@@ -50,19 +62,25 @@ suma_lecciones :-
         ),
         Sumas
     ),
-    Sumas \= [], !,
-    format('Error: suma de lecciones por nivel distinta de lecc_por_sem: ~w~n', [Sumas]),
-    fail.
-suma_lecciones.
+    ( Sumas = [] ->
+        true
+    ;   format('Error: suma de lecciones por nivel distinta de lecc_por_sem: ~w~n', [Sumas]),
+        fail
+    ).
+
+
+% req_sin_profesor(?Nivel, ?Materia)
+% Nivel tiene un requisito de Materia sin profesor asignado.
+req_sin_profesor(Nivel, Materia) :-
+    req(Nivel, Materia, _),
+    \+ prof(_, Nivel, Materia).
 
 
 % a ningún requisito debe faltarle un profesor que lo pueda cubrir
 todo_req_con_profesor :-
     findall(
         (Nivel, Materia),
-        (   req(Nivel, Materia, _),
-            \+ prof(_, Nivel, Materia)
-        ),
+        req_sin_profesor(Nivel, Materia),
         Reqs
     ),
     Reqs \= [], !,
@@ -72,7 +90,7 @@ todo_req_con_profesor.
 
 
 % prof_cubriendo_lecciones(+Profesor, ?Cantidad)
-% Cantidad es la cantidad de lecciones que el Profesor puede cubrir.
+% Cantidad es la cantidad de lecciones que el Profesor debe cubrir.
 prof_cubriendo_lecciones(Profesor, Cantidad) :-
     findall(
         Cant,
@@ -96,7 +114,7 @@ prof_disponible(Profesor, Cantidad) :-
 
 
 % ningún profesor debe estar asignado a una cantidad de materias mayor a la que puede cubrir
-ningun_profesor_mayor_a_cubrir :-
+ningun_profesor_sin_poder_cubrir :-
     findall(
         (Profesor, CantReq, CantidadDispo),
         (   prof(Profesor, _Nivel, _Materia),
@@ -109,4 +127,4 @@ ningun_profesor_mayor_a_cubrir :-
     ProfCant \= [], !,
     format('Error: profesor(es) con cantidad de materias mayor a lo que puede cubrir: ~w~n', [ProfCant]),
     fail.
-ningun_profesor_mayor_a_cubrir.
+ningun_profesor_sin_poder_cubrir.
