56bfd1b8ff69ba8530adf53da7ea07098d6f01a6
Carlos A. Lang-Sanou <carlos.lang@gmail.com>
2025-08-25 16:42:10 -0600

    fix rectangles

--- sandbox/rectangles.pl
+++ sandbox/rectangles.pl
@@ -14,39 +14,72 @@ test3 :-
 test4 :-
     disjoint2([a(0,1,1,1), b(0,1,1,1)]).
 
-% profX(Grupo, cant_de _grupos=1, Lecc, cant_de_lecc=1).
-% profX(Grupo, 1, Lecc, 1).
+% ProfX(Grupo, cant_de _grupos=1, Lecc, cant_de_lecc=1).
+% ProfX(Grupo, 1, Lecc, 1).
 cases(Cs) :-
     Cs = [
-        prof1(G1,1,L1,1),
-        prof1(G1,1,L2,1),
-        prof1(G1,1,L3,1),
-        prof1(G2,1,L4,1),
-        prof1(G2,1,L5,1),
-        prof1(G2,1,L6,1),
-        prof2(G1,1,L7,1),
-        prof2(G1,1,L8,1),
-        prof2(G1,1,L9,1),
-        prof2(G2,1,L10,1),
-        prof2(G2,1,L11,1),
-        prof2(G2,1,L12,1)
+        prof1(1,1,L1,1),
+        prof1(1,1,L2,1),
+        prof1(1,1,L3,1),
+        prof1(2,1,L4,1),
+        prof1(2,1,L5,1),
+        prof1(2,1,L6,1),
+        prof2(1,1,L7,1),
+        prof2(1,1,L8,1),
+        prof2(1,1,L9,1),
+        prof2(2,1,L10,1),
+        prof2(2,1,L11,1),
+        prof2(2,1,L12,1)
     ],
     % [G1,G2,G3,G4,G5,G6] ins 0..1,
-    [G1,G2] ins 0..1,
-    [L1,L2,L3,L4,L5,L6,L7,L8,L9,L10,L11,L12] ins 0..5,
-    L1 #< L2, L2 #< L3, L4 #< L5, L5 #< L6,
-    L7 #< L8, L8 #< L9, L10 #< L11, L11 #< L12.
+    % [G1,G2] ins 0..1,
 
-main(Cs) :- 
+    % define lesson variables domain
+    maplist(lecc_in, Cs, Lecc_List),
+    Lecc_List ins 0..5,
+    % [L1,L2,L3,L4,L5,L6,L7,L8,L9,L10,L11,L12] ins 0..5,
+    % separate_prof_lessons(Cs),
+    L1 #< L2, L2 #< L3, L4 #< L5, L5 #< L6,
+    L7 #< L8, L8 #< L9, L10 #< L11, L11 #< L12,
+    all_distinct([L1,L2,L3,L4,L5,L6]),
+    all_distinct([L7,L8,L9,L10,L11,L12]),
+    all_distinct([L1,L2,L3,L7,L8,L9]),
+    all_distinct([L4,L5,L6,L10,L11,L12]).
+
+main :- 
     cases(Cs),
+    % constrain_cases(Cs),
     % format('Cs1: ~w~n', [Cs]),
     disjoint2(Cs),
     % format('Cs2: ~w~n', [Cs]),
-    vars_in(Cs, Var_List),
-    % format('Var_List: ~w~n', [Var_List]),
-    label(Var_List),
+    leccs_in(Cs, Lecc_List),
+    % format('Lecc_List: ~w~n', [Lecc_List]),
+    label(Lecc_List),
     print_schedule(Cs).
     % format('Cs3: ~w~n', [Cs]).
+
+
+profs_in(Cs, Var_List) :-
+    maplist(prof_in, Cs, Var_List).
+
+groups_in(Cs, Var_List) :-
+    maplist(group_in, Cs, Var_List).
+
+leccs_in(Cs, Var_List) :-
+    maplist(lecc_in, Cs, Var_List).
+
+
+prof_in(Term, Prof) :-
+    Term =.. [Prof, _Group, _, _Lecc, _].
+
+group_in(Term, Group) :-
+    Term =.. [_Func, Group, _, _Lecc, _].
+
+lecc_in(Term, Lecc) :-
+    Term =.. [_Func, _Group, _, Lecc, _].
+
+
+
 
 print_schedule(Cs) :-
     maplist(=.., Cs, List),
@@ -57,28 +90,18 @@ print_schedule(Cs) :-
         Schedule
     ),
     sort(Schedule, Sorted_Schedule),
-    format('Sorted_Schedule: ~w~n', [Sorted_Schedule]),
+    % format('Sorted_Schedule: ~w~n', [Sorted_Schedule]),
     findall(Prof, member([Prof, _, _], Sorted_Schedule), Profs),
     sort(Profs, Unique_Profs),
     forall(
         member(Prof, Unique_Profs),
         (   format('Profesor: ~w~nGrupos:', [Prof]),
             forall(
-                member([Prof, _Lecc, Grupo], Sorted_Schedule),
-                format('\t~w', [Grupo])
+                member([Prof, Lecc, Grupo], Sorted_Schedule),
+                format('\t~w:~w', [Lecc,Grupo])
             ),
             nl
         )
     ).
 
-vars_in(Cs, Var_List) :-
-    maplist(group_var_in, Cs, Group_List),
-    maplist(lecc_var_in, Cs, Lecc_List),
-    append(Group_List, Lecc_List, Var_List).
-
-group_var_in(Term, Group) :-
-    Term =.. [_Func, Group, _, _Lecc, _].
-
-lecc_var_in(Term, Lecc) :-
-    Term =.. [_Func, _Group, _, Lecc, _].
 
